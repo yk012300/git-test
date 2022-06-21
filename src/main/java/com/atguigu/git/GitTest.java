54134184fc8f64fc248ceaf5c135a462e54713b2
yk012300 <107901540+yk012300@users.noreply.github.com>
2022-06-21 17:30:51 +0800

    use idea pull project

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("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");
         System.out.println("use idea push ");
+        System.out.println("idea pull push ");
     }
 }
